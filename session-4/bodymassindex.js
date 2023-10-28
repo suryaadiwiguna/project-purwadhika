5e304cc7b563c99c2966cd25cf9545d8e4e61944
suryaadiwiguna <suryaadiwiguna@gmail.com>
2023-10-28 09:32:52 +0700

    fix: Handling infinity result

--- session-4/bodymassindex.js
+++ session-4/bodymassindex.js
@@ -1,16 +1,16 @@
-function calculateBMI(weight, height){
+function calculateBMI(weight, height) {
     let bmiCategory;
     let rawBMI = weight / (height * height);;
     let bmi = rawBMI.toFixed(2);
-    if (bmi <18.5){
+    if (bmi < 18.5) {
         bmiCategory = "less weight";
-    } else if (bmi <24.9) {
+    } else if (bmi < 24.9) {
         bmiCategory = "ideal";
     } else if (bmi < 29.9) {
         bmiCategory = "overweight";
-    } else if (bmi < 39.9){
+    } else if (bmi < 39.9) {
         bmiCategory = "very overweight";
-    } else if (bmi >= 39.9 ){
+    } else if (bmi >= 39.9 && bmi != Infinity) {
         bmiCategory = "obesity";
     } else {
         bmiCategory = "error";
@@ -18,4 +18,4 @@ function calculateBMI(weight, height){
     return `Your BMI is ${bmi}, categorized as ${bmiCategory}.`;
 }
 
-console.log(calculateBMI(82, 1.78));
+console.log(calculateBMI(1, 0));
